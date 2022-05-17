41c08df45af0bc99f907150eaa3e5175043d4803
Mon <91760182+thismn@users.noreply.github.com>
2022-05-17 12:54:26 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n • Channel : -- \n • Source Code: <a href='https://github.com'>Github</a></b>\n",
+            text=f"<b>@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator : @{OWNER}\n • Channel : @yaagtau \n • Source Code : <a href='https://github.com'>Klik Disini</a></b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
